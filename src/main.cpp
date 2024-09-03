1b973dafe124002cd6e6f5bfa59fb878078b6369
Hadebg <85008919+Hadebg@users.noreply.github.com>
2024-09-03 12:49:34 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -93,7 +93,7 @@ void loop(){
     else {
       Serial.println("Password is incorrect!");
       lcd.print("Sai! Thu lai");
-      delay(1000);
+      delay(3000);
     }
     lcd.clear();
     clearData();
