5c79efd7d0eda6ced72c03eb2bcad65fef896381
Hadebg <85008919+Hadebg@users.noreply.github.com>
2024-09-03 09:44:15 +0700

    Update main.h

--- src/main.h
+++ src/main.h
@@ -26,19 +26,19 @@ byte data_count = 0;
 
 //Represent keys on keypad
 char hexaKeys[ROWS][COLS] = {
-    {'1', '2', '3'},
-    {'4', '5', '6'},
-    {'7', '8', '9'},
-    {'*', '0', '#'}
+  {'1', '2', '3', 'A'},
+  {'4', '5', '6', 'B'},
+  {'7', '8', '9', 'C'},
+  {'*', '0', '#', 'D'},
 };
 
 //Connections to Arduino
-byte rowPins[ROWS] = {21, 19, 18, 5};
-byte colPins[COLS] = {17, 16, 4};
+byte rowPins[ROWS] = {19, 18, 5, 17};
+byte colPins[COLS] = {16, 4, 2, 15};
 
-#define Password_Length 7
+#define Password_Length 8
 char Data[Password_Length];
-char Master[Password_Length] = "123456";
+char Master[Password_Length] = "123A456";
 
 void clearData();
 
